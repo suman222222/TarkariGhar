c16f2cab7838200d33cfea39b0e05b30e2b41195
Suman Kapri <kaprisuman222@gmail.com>
2026-06-08 22:33:58 +0545

    Add profile page and homepage improvements

--- js/cart.js
+++ js/cart.js
@@ -1,14 +1,35 @@
-let cart = JSON.parse(localStorage.getItem("cart")) || [];
+let cart =
+JSON.parse(localStorage.getItem("cart")) || [];
 
-function addToCart(name, price) {
+function addToCart(name,price){
+
+    const loggedIn =
+    localStorage.getItem("loggedIn");
+
+    if(!loggedIn){
+
+        alert(
+            "Please login before adding products to cart."
+        );
+
+        window.location.href =
+        "login.html";
+
+        return;
+    }
+
     const item = {
-        name: name,
-        price: price
+        name:name,
+        price:price
     };
 
     cart.push(item);
 
-    localStorage.setItem("cart", JSON.stringify(cart));
+    localStorage.setItem(
+        "cart",
+        JSON.stringify(cart)
+    );
 
     alert(name + " added to cart!");
+
 }
